60fbdfe4b1740595be3b707e0ce5bbad4aecf653
EMPaThy789 <fyempathy@gmail.com>
2017-05-23 22:08:14 +1200

    added experiment script and generators from Jean Paul. Also modified ISS classifiers to have a seperate decay interval instead of decay being tied to re-ranking.

--- moa/src/main/java/build.sh
+++ moa/src/main/java/build.sh
@@ -1,31 +1,42 @@
-javac -cp moa.jar moa/core/Utils.java
+javac -encoding utf8 -cp moa.jar moa/core/Utils.java
 jar -uf  moa.jar  moa/core/Utils.class 
 rm moa/core/Utils.class 
 
-javac -cp  moa.jar moa/classifiers/lazy/neighboursearch/EuclideanDistance.java
+javac -encoding utf8 -cp  moa.jar moa/classifiers/lazy/neighboursearch/EuclideanDistance.java
 jar -uf  moa.jar moa/classifiers/lazy/neighboursearch/EuclideanDistance.class
 rm moa/classifiers/lazy/neighboursearch/EuclideanDistance.class
 
-javac -cp  moa.jar moa/classifiers/lazy/neighboursearch/CumulativeLinearNNSearch.java
+javac -encoding utf8 -cp  moa.jar moa/classifiers/lazy/neighboursearch/CumulativeLinearNNSearch.java
 jar -uf  moa.jar moa/classifiers/lazy/neighboursearch/*.class
 rm moa/classifiers/lazy/neighboursearch/*.class
 
-
-javac -cp  moa.jar moa/classifiers/lazy/rankingfunctions/*.java
+javac -encoding utf8 -cp  moa.jar moa/classifiers/lazy/rankingfunctions/*.java
 jar -uf  moa.jar moa/classifiers/lazy/rankingfunctions/*.class
 rm moa/classifiers/lazy/rankingfunctions/*.class
 
-javac -cp  moa.jar moa/classifiers/bayes/NaiveBayesISS.java
+javac -encoding utf8 -cp  moa.jar moa/classifiers/lazy/kNNISS.java
+jar -uf  moa.jar moa/classifiers/lazy/kNNISS.class
+rm moa/classifiers/lazy/*.class
+
+javac -encoding utf8 -cp  moa.jar moa/classifiers/bayes/NaiveBayesISS.java
 jar -uf  moa.jar moa/classifiers/bayes/NaiveBayesISS.class
 rm moa/classifiers/bayes/NaiveBayesISS.class
 
-javac -cp  moa.jar moa/classifiers/lazy/neighboursearch/EuclideanDistance.java
+javac -encoding utf8 -cp  moa.jar moa/classifiers/lazy/neighboursearch/EuclideanDistance.java
 jar -uf  moa.jar moa/classifiers/lazy/neighboursearch/EuclideanDistance.class
 rm moa/classifiers/lazy/neighboursearch/EuclideanDistance.class
 
-javac -cp moa.jar moa/streams/generators/ConditionalGenerator.java
+javac -encoding utf8 -cp moa.jar moa/streams/generators/ConditionalGenerator.java
+javac -encoding utf8 -cp moa.jar moa/streams/generators/BG.java
+javac -encoding utf8 -cp moa.jar moa/streams/generators/BG2.java
+javac -encoding utf8 -cp moa.jar moa/streams/generators/BG3.java
+javac -encoding utf8 -cp moa.jar moa/streams/generators/SEAFD.java
+javac -encoding utf8 -cp moa.jar moa/streams/generators/AssetNegotiationGenerator.java
 jar -uf  moa.jar moa/streams/generators/*.class
 rm moa/streams/generators/*.class
 
+javac -encoding utf8 -cp  moa.jar moa/ISSExperiments.java
+jar -uf  moa.jar moa/ISSExperiments.class
+rm moa/ISSExperiments.class
 
 read -rsp $'Press enter to continue...\n'
